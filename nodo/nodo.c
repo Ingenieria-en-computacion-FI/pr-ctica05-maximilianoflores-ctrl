ccc870eee13c3c3bb4e9bc55fedc41cabc73c96b
maximilianoflores-ctrl <matstito7002@gmail.com>
2026-03-30 18:33:27 +0000

    subo nodo

--- nodo/nodo.c
+++ nodo/nodo.c
@@ -1,11 +1,19 @@
 #include <stdlib.h>
 #include "nodo.h"
+#include <stdio.h>
 
-Nodo* nodo_crear(int dato)
-{
-    // Todo: implementar la función }
+Nodo* nodo_crear(int dato){
+    Nodo* nuevoNodo = (Nodo*)malloc(sizeof(Nodo));
+    if(nuevo Nodo !=NULL) {
+        nuevoNodo -> dato= dato;
+        nuevoNodo->siguiente=NULL;
+        return nuevoNodo;
+    }
+ 
 
 void nodo_destruir(Nodo* nodo)
 {
-    // Todo: implementar la función 
+    if(nodo !=NULL){
+        free(nodo);
+    }
 }
